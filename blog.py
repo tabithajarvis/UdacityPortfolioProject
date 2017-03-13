c06aae8760793ac8bd5adc904c8908314fc405f0
Tabitha Jarvis <Tabitha Jarvis>
2017-03-13 02:26:06 -0600

    Removing bad CSS, renamed blog html page, added handling for reaching Blog page and Add New Blog Post page.

--- blog.py
+++ blog.py
@@ -13,7 +13,7 @@ def render_str(template, **params):
     t = jinja_env.get_template(template)
     return t.render(params)
 
-class BlogHandler(webapp2.RequestHandler):
+class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
@@ -23,11 +23,36 @@ class BlogHandler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
-
-class MainPage(BlogHandler):
+# Page Handlers
+class MainPage(Handler):
     def get(self):
         self.render('index.html')
 
+class BlogPage(Handler):
+    def get(self):
+        posts = db.GqlQuery("select * from Post order by created desc limit 10")
+        self.render('blog.html', posts = posts)
+
+class NewPost(Handler):
+    def get(self):
+        self.render('newpost.html')
+
+    def post(self):
+        self.render('newpost.html')
+
+# Database Tables
+class Post(db.Model):
+    subject = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+    last_modified = db.DateTimeProperty(auto_now=True)
+
+    def render(self):
+        self.__render_text = self.content.replace('\n', '<br>')
+        return render_str("post.html", p=self)
+
 app = webapp2.WSGIApplication([('/', MainPage),
+                               ('/blog', BlogPage),
+                               ('/newpost', NewPost),
                                ],
                               debug=True)
